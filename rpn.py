a7192ed959b689cc3b936e93165d57e0b099950c
Gregory <gnoma@umich.edu>
2017-10-27 12:19:09 -0400

    Implement ADD (+)

--- rpn.py
+++ rpn.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    print(stack)
+    return stack.pop()
 
 def main():
     while True:
@@ -9,3 +19,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
